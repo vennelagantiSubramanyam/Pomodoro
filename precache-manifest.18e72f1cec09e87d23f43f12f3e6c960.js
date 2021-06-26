0e116b0174fea46bd4cec58a6aa015875b8b9b39
vennelagantiSubramanyam <vennelagantisubbu@gmail.com>
2021-06-26 16:04:12 +0530

    Updates

---
+++ precache-manifest.18e72f1cec09e87d23f43f12f3e6c960.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea280817572c27f9f1ddaa5faf6c89a",
+    "url": "/Pomodoro/index.html"
+  },
+  {
+    "revision": "edf1af0023b2f875a93a",
+    "url": "/Pomodoro/static/css/main.d2ce90d3.chunk.css"
+  },
+  {
+    "revision": "a83b866c1c0f576d1986",
+    "url": "/Pomodoro/static/js/2.5b385fb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro/static/js/2.5b385fb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf1af0023b2f875a93a",
+    "url": "/Pomodoro/static/js/main.62a0fa08.chunk.js"
+  },
+  {
+    "revision": "73df80a2b482a43907e9",
+    "url": "/Pomodoro/static/js/runtime-main.556a2e70.js"
+  }
+]);
